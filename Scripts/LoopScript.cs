927bab15c9c725568157bc3f947e8888ab16005a
m-vishnuraj <mvishnurajm@gmail.com>
2023-08-01 16:55:12 +0530

    while loop

--- Scripts/LoopScript.cs
+++ Scripts/LoopScript.cs
@@ -7,9 +7,18 @@ public class LoopScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // for loop
         for(int i=1; i<=5; i++)
         {
             print("Ram");
+        }
+
+        // while loop
+        int j = 1;
+        while(j<=5)
+        {
+            print("Shyam");
+            j++;
         }
     }
 
