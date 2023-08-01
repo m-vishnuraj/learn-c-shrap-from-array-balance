1640f6ef6163329a52e377155b59804d324446e7
m-vishnuraj <mvishnurajm@gmail.com>
2023-08-01 11:13:19 +0530

    array

--- Scripts/ArrayScript.cs
+++ Scripts/ArrayScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ArrayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] objects;
+
+    public Color[] colors;
+
     void Start()
     {
-        
+        // Destroy (objects[0], 2f);
+        objects[2].GetComponent<Renderer>().material.color = colors[1];
+        objects[0].GetComponent<Renderer>().material.color = colors[3];
+        objects[3].GetComponent<Renderer>().material.color = colors[0];
+        objects[1].GetComponent<Renderer>().material.color = colors[2];
     }
 
-    // Update is called once per frame
     void Update()
     {
         
